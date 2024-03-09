4abae27f9dcd0300855e6225106f8f018eeddafa
disisdsid <136875283+disisdsid@users.noreply.github.com>
2024-03-09 23:28:59 +0530

    Create test.py

---
+++ .github/workflows/test.py
@@ -0,0 +1,8 @@
+# test_my_math.py
+
+from my_math import add_numbers
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
